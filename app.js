9f0bba4ee7cea234efdcc7adb2cad42c931fa0db
Spacemahn <tevin2828@gmail.com>
2023-09-05 14:06:48 -0400

    Add ui and eliminate console logs for easier user input.
    Change function playGame to checkScore due to console functionality no longer being needed. Add buttons and text to HTML. Use DOM to add functionality to buttons and textContent of text elements.

--- app.js
+++ app.js
@@ -1,66 +1,108 @@
 let playerScore = 0;
 let computerScore = 0;
 
-function getComputerChoice () {
-    compChoices = ['rock', 'paper', 'scissors']
-    randomItem = compChoices[Math.floor(Math.random()*compChoices.length)];
-    return randomItem;
-}   
+rock_btn = document.querySelector('#rock_btn');
+paper_btn = document.querySelector('#paper_btn');
+scissors_btn = document.querySelector('#scissors_btn');
+buttons = document.querySelectorAll('button');
+what_hand_msg = document.querySelector('h2');
 
-function playRound (playerSelection, computerSelection) {
+score_msg = document.querySelector('#score_msg');
+player_score_counter = document.querySelector('#player_score_counter');
+computer_score_counter = document.querySelector('#computer_score_counter');
+
+rock_btn.addEventListener('click', function () {
+    playRound('rock', getComputerChoice())
+}
+);
+paper_btn.addEventListener('click', function () {
+    playRound('paper', getComputerChoice())
+});
+scissors_btn.addEventListener('click', function () {
+    playRound('scissors', getComputerChoice())
+})
+
+
+function getComputerChoice() {
+    compChoices = ['rock', 'paper', 'scissors']
+    randomItem = compChoices[Math.floor(Math.random() * compChoices.length)];
+    return randomItem;
+}
+
+function updateScoreCounter() {
+    player_score_counter.textContent = playerScore;
+    computer_score_counter.textContent = computerScore;
+}
+
+
+function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        return `Tie! you both chose ${playerSelection}`
+        score_msg.textContent = `Tie! you both chose ${playerSelection}`
+        updateScoreCounter();
     }
     else if (playerSelection === 'rock') {
         if (computerSelection === 'paper') {
             computerScore += 1;
-            return `You lose! ${computerSelection} beats ${playerSelection}`
+            updateScoreCounter();
+            score_msg.textContent = `You lose! ${computerSelection} beats ${playerSelection}`
+            checkScore();
         }
         if (computerSelection === 'scissors') {
             playerScore += 1;
-            return `You win! ${playerSelection} beats ${computerSelection}`
+            updateScoreCounter();
+            score_msg.textContent = `You win! ${playerSelection} beats ${computerSelection}`
+            checkScore();
         }
     }
     else if (playerSelection === 'paper') {
         if (computerSelection === 'scissors') {
             computerScore += 1;
-            return `You lose! ${computerSelection} beats ${playerSelection}`
-        } 
+            updateScoreCounter();
+            score_msg.textContent = `You lose! ${computerSelection} beats ${playerSelection}`
+            checkScore();
+        }
         if (computerSelection === 'rock') {
             playerScore += 1;
-            return `You win! ${playerSelection} beats ${computerSelection}`
+            updateScoreCounter();
+            score_msg.textContent = `You win! ${playerSelection} beats ${computerSelection}`
+            checkScore();
         }
     }
     else if (playerSelection === 'scissors') {
         if (computerSelection === 'rock') {
             computerScore += 1;
-            return `You lose! ${computerSelection} beats ${playerSelection}`
+            updateScoreCounter();
+            score_msg.textContent = `You lose! ${computerSelection} beats ${playerSelection}`
+            checkScore();
         }
         if (computerSelection === 'paper') {
             playerScore += 1;
-            return `You win! ${playerSelection} beats ${computerSelection}`
+            updateScoreCounter();
+            score_msg.textContent = `You win! ${playerSelection} beats ${computerSelection}`
+            checkScore();
         }
     }
 }
 
-function game() {
-    while (playerScore < 5 && computerScore < 5) {
-        let playerSelection = (prompt('What hand do you want to play?')).toLowerCase();
-        console.log(playRound(playerSelection, getComputerChoice()))
-        console.log('Current score:');
-        console.log(`Player: ${playerScore} Computer ${computerScore}`);
-    }
+function checkScore() {
     if (playerScore === 5) {
-        console.log('You win! Congratulations');
+        score_msg.textContent = 'YOU WIN THE GAME! Congratulations!'
+        buttons.forEach(button => button.disabled = true)
     }
     else if (computerScore === 5) {
-        console.log('Sorry, you lose!');
+        score_msg.textContent = 'Sorry, you lost the game! :('
+        buttons.forEach(button => button.disabled = true)
     }
 }
 
-console.log('Welcome to Rock, Paper, Scissors!');
-console.log("Let's play to 5!")
-game();
+
+setTimeout(function () {
+    what_hand_msg.textContent = "What hand would you like to play?"
+}, 2500)
+
+
+
+// playGame();
 
 
 
